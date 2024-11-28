d61f2c07e33b56a9d88c1e7dcddc630a93cdd4c2
joel reynaga <joel.reynaga@uni9.edu.br>
2024-11-28 13:35:45 -0300

    adicionando novo arquivo na pasta

---
+++ DDL/DDL/tabela_restau.sql
@@ -0,0 +1,11 @@
+use rest;
+create table restau(
+id_restaur INT  auto_increment primary key,
+cliente varchar(50),
+mensagem_c varchar(100),
+nota_de_atendimento decimal(2,0),
+data_de_atendimento date
+);
+
+
+
